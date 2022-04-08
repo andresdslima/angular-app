9976ff902d4c36a5e709cfe54649f8c5571f2533
Andre Lima <andresdslima@gmail.com>
2022-04-09 00:17:31 +0200

    fixing bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,24 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  palavra = 'HIPERTROFIA-MUSCULAR';
+  palavra = 'PROGRAMMING';
   palavraOculta = '';
   tentativas = 0;
   ganhei = false;
   perdi = false;
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-            'T', 'U', 'V', 'X', 'W', 'Y', 'Z', '-'];
-  
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'X', 'W', 'Y', 'Z', '-'];
+
   constructor() {
-    this.palavraOculta = '_ '.repeat( this.palavra.length );
+    this.palavraOculta = '_ '.repeat(this.palavra.length);
   }
 
-  comprovar( letra: string ) {
-    this.existeLetra( letra );
+  comprovar(letra: string) {
+    this.existeLetra(letra);
     const palavraOcultaArr = this.palavraOculta.split(' ');
-    for( let i=0; i < this.palavra.length; i++ ) {
-      if( this.palavra[i] == letra ) {
+    for (let i = 0; i < this.palavra.length; i++) {
+      if (this.palavra[i] == letra) {
         palavraOcultaArr[i] = letra;
       }
     }
@@ -31,20 +31,20 @@ export class AppComponent {
     this.verificaGanhei();
   }
 
-  existeLetra ( letra: string) {
-    if( this.palavra.indexOf( letra ) >= 0) {}
+  existeLetra(letra: string) {
+    if (this.palavra.indexOf(letra) >= 0) { }
     else {
-      this.tentativas ++;
+      this.tentativas++;
     }
   }
 
   verificaGanhei() {
     const palavraArr = this.palavraOculta.split(' ');
     const palavraAval = palavraArr.join('');
-    if( palavraAval == this.palavra) {
+    if (palavraAval == this.palavra) {
       this.ganhei = true;
     }
-    if( this.tentativas >= 9) {
+    if (this.tentativas >= 9) {
       this.perdi = true;
     }
   }
