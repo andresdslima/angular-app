af8c88150c9ae245e80d215fc89fba697bf7544c
Andre Lima <andresdslima@gmail.com>
2022-08-17 20:42:00 +0200

    fix button selection (once only)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  title = 'ahorcado';
   palavra = 'PROGRAMMING';
   palavraOculta = '';
   tentativas = 0;
@@ -14,6 +15,7 @@ export class AppComponent {
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'X', 'W', 'Y', 'Z', '-'];
+  errors = '';
 
   constructor() {
     this.palavraOculta = '_ '.repeat(this.palavra.length);
@@ -22,28 +24,34 @@ export class AppComponent {
   comprovar(letra: string) {
     this.existeLetra(letra);
     const palavraOcultaArr = this.palavraOculta.split(' ');
+
     for (let i = 0; i < this.palavra.length; i++) {
       if (this.palavra[i] == letra) {
         palavraOcultaArr[i] = letra;
       }
     }
+
     this.palavraOculta = palavraOcultaArr.join(' ');
     this.verificaGanhei();
   }
 
   existeLetra(letra: string) {
-    if (this.palavra.indexOf(letra) >= 0) { }
-    else {
+    if (this.palavra.indexOf(letra) < 0) {
+      if (this.errors.indexOf(letra) >= 0) return;
+
       this.tentativas++;
+      this.errors += letra + ', ';
     }
   }
 
   verificaGanhei() {
     const palavraArr = this.palavraOculta.split(' ');
     const palavraAval = palavraArr.join('');
+
     if (palavraAval == this.palavra) {
       this.ganhei = true;
     }
+
     if (this.tentativas >= 9) {
       this.perdi = true;
     }
